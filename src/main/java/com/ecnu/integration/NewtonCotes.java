904e48c88f8600ff31151c78fe4b72343b372204
Slev <H138082@outlook.com>
2026-05-28 18:57:54 +0800

    修正NewtonCotes，增加参数，并重新进行测试

--- src/main/java/com/ecnu/integration/NewtonCotes.java
+++ src/main/java/com/ecnu/integration/NewtonCotes.java
@@ -3,12 +3,24 @@ package com.ecnu.integration;
 import com.ecnu.Function;
 
 public class NewtonCotes {
-    public double trapezoidal(Function f, double a, double b) {
-        return (f.eval(a) + f.eval(b)) * (b - a) / 2.0;
+    public double trapezoidal(Function f, double a, double b, int n) {
+        double h = (b - a) / n;
+        double sum = 0.5 * (f.eval(a) + f.eval(b));
+        for (int i = 1; i < n; i++) {
+            sum += f.eval(a + i * h);
+        }
+        return sum * h;
     }
 
-    public double simpson(Function f, double a, double b) {
-        double c = (a + b) / 2.0;
-        return (f.eval(a) + 4.0 * f.eval(c) + f.eval(b)) * (b - a) / 6.0;
+    public double simpson(Function f, double a, double b, int n) {
+        double h = (b - a) / n;
+        double sum = f.eval(a) + f.eval(b);
+        for (int i = 1; i < n; i += 2) {
+            sum += 4 * f.eval(a + i * h);
+        }
+        for (int i = 2; i < n; i += 2) {
+            sum += 2 * f.eval(a + i * h);
+        }
+        return sum * h / 3.0;
     }
 }
--- src/test/java/com/ecnu/integration/NewtonCotesTest.java
+++ src/test/java/com/ecnu/integration/NewtonCotesTest.java
@@ -7,19 +7,19 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class NewtonCotesTest {
-    private final NewtonCotes newtonCatos = new NewtonCotes();
+    private final NewtonCotes newtonCotes = new NewtonCotes();
 
     @Test
     public void testTrapezoidalRule() {
         Linear function = new Linear(2.0, 3.0);
 
-        assertEquals(10.0, newtonCatos.trapezoidal(function, 0.0, 2.0), 1e-10);
+        assertEquals(10.0, newtonCotes.trapezoidal(function, 0.0, 2.0, 10), 1e-10);
     }
 
     @Test
     public void testSimpsonRuleForSineIntegral() {
         Sin function = new Sin(1.0, 0.0);
 
-        assertEquals(2.0 * Math.PI / 3.0, newtonCatos.simpson(function, 0.0, Math.PI), 1e-10);
+        assertEquals(2.0, newtonCotes.simpson(function, 0.0, Math.PI, 100), 1e-7);
     }
 }
